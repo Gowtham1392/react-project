fa0c49b1f1088017b800369aaea7d1f1d0769db1
Gowtham M <gowtham.1392@gmail.com>
2019-07-16 20:39:19 +0530

    Data passed to the child components with the help of consumer

--- src/Context.js
+++ src/Context.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
 
 const productContext = React.createContext();
 class ProductProvider extends Component {
-  state = {};
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
+
+  handleDetail = () => {
+    console.log("Hello From handleDetail");
+  };
+  addToCart = () => {
+    console.log("Hello From addToCart");
+  };
+
   render() {
     return (
-      <productContext.Provider value="Hello From Context">
+      <productContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
         {this.props.children}
       </productContext.Provider>
     );
   }
 }
 
-const productConsumer = productContext.Consumer;
+const ProductConsumer = productContext.Consumer;
 
-export default { ProductProvider, productConsumer };
+export { ProductProvider, ProductConsumer };
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
 import { storeProducts } from "../data";
+import { ProductConsumer } from "../Context";
 
 class ProductList extends Component {
   state = {
@@ -13,7 +14,15 @@ class ProductList extends Component {
         <div className="py-5">
           <div className="container">
             <Title name="Our" title="Products" />
-            <div className="row" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
           </div>
           {/* <Product /> */}
         </div>
