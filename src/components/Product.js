1411e47479e378245521a0751f6ad9579a35cd1a
Gowtham M <gowtham.1392@gmail.com>
2019-07-17 19:24:05 +0530

    product data is displayed in product list page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../Context";
 
 class Product extends Component {
   state = {};
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
-        <h2>Hello From Product</h2>
-      </div>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={console.log("container clicked")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top" />
+            </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                console.log("added to the cart");
+              }}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  in cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
+          </div>
+        </div>
+      </ProductWrapper>
     );
   }
 }
-
+const ProductWrapper = styled.div``;
 export default Product;
