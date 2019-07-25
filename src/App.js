4b5c607c7f120a3427d19f491f655aa6578a1bae
Gowtham M <gowtham.1392@gmail.com>
2019-07-25 19:32:04 +0530

    Context.js is Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
-import Cart from "./components/Cart";
 import Default from "./components/Default";
-
-class App extends React.Component {
+import Cart from "./components/Cart";
+import Modal from "./components/Modal";
+class App extends Component {
   render() {
     return (
       <React.Fragment>
@@ -18,9 +17,9 @@ class App extends React.Component {
           <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />
           <Route path="/cart" component={Cart} />
-          {/* <Route path="/cart" Component={Cart} /> */}
           <Route component={Default} />
         </Switch>
+        <Modal />
       </React.Fragment>
     );
   }
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import { ProductConsumer } from "../Context";
-import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 import { ButtonContainer } from "./Button";
-
-class Details extends Component {
-  state = {};
+import { Link } from "react-router-dom";
+export default class Details extends Component {
   render() {
     return (
       <ProductConsumer>
@@ -18,43 +16,50 @@ class Details extends Component {
             title,
             inCart
           } = value.detailProduct;
+
           return (
             <div className="container py-5">
+              {/* title */}
               <div className="row">
                 <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
                   <h1>{title}</h1>
                 </div>
               </div>
+              {/* end of title */}
               <div className="row">
                 <div className="col-10 mx-auto col-md-6 my-3">
-                  <img src={img} className="img-fluid" alt="Product" />
+                  <img src={img} className="img-fluid" alt="" />
                 </div>
+                {/* prdoduct info */}
                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                  <h2>Model: {title}</h2>
+                  <h1>model : {title}</h1>
                   <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
-                    Made By: <span className="text-uppercase">{company}</span>
+                    made by : <span className="text-uppercase">{company}</span>
                   </h4>
                   <h4 className="text-blue">
                     <strong>
-                      price: <span>$</span>
+                      price : <span>$</span>
                       {price}
                     </strong>
                   </h4>
                   <p className="text-capitalize font-weight-bold mt-3 mb-0">
-                    some info about the product:
+                    some info about product :
                   </p>
                   <p className="text-muted lead">{info}</p>
+                  {/* buttons */}
                   <div>
                     <Link to="/">
-                      <ButtonContainer>Back to Products</ButtonContainer>
+                      <ButtonContainer>back to products</ButtonContainer>
                     </Link>
                     <ButtonContainer
+                      cart
                       disabled={inCart ? true : false}
                       onClick={() => {
                         value.addToCart(id);
+                        value.openModal(id);
                       }}
                     >
-                      {inCart ? "In Cart" : "Add to Cart"}
+                      {inCart ? "in cart" : "add to cart"}
                     </ButtonContainer>
                   </div>
                 </div>
@@ -66,5 +71,3 @@ class Details extends Component {
     );
   }
 }
-
-export default Details;
